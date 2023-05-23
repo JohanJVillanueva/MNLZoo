e3829bc1b5f0bcf36721b19acb7d2fd58a901958
JohanJVillanueva <johanjvillanueva@iskolarngbayan.edu.ph>
2023-05-23 14:05:43 +0800

    good home

--- functions.js
+++ functions.js
@@ -14,27 +14,3 @@ function play() {
 
 
 /*slide*/
-let currentSlide = 0;
-
-function startSlider() {
-    let imageCount = document.getElementById("img1");
-  
-    if (imageCount.length === 0) {
-      imageCount = document.getElementById("img1");
-      images.style.transform = `translateX(0px)`;
-      return;
-    }
-  
-    let images = document.getElementById("ul1");
-    images.style.transform = `translateX(-${currentSlide * 450}px)`;
-  
-    if (currentSlide === imageCount.length - 1) {
-      currentSlide = 0;
-    } else {
-      currentSlide++;
-    }
-  }
-  
-  setInterval(() => {
-    startSlider();
-  }, 5000);
