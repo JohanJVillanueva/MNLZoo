96c03227d63a0d99e1cb74fd4945c47c35647ea6
JohanJVillanueva <johanjvillanueva@iskolarngbayan.edu.ph>
2023-05-23 13:46:25 +0800

    Second Ever commit

--- functions.js
+++ functions.js
@@ -4,5 +4,37 @@ function topFunction() {
 
 function play() {
     var audio = document.getElementById("audio");
-    audio.play();
-}
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  }
+
+
+
+/*slide*/
+let currentSlide = 0;
+
+function startSlider() {
+    let imageCount = document.getElementById("img1");
+  
+    if (imageCount.length === 0) {
+      imageCount = document.getElementById("img1");
+      images.style.transform = `translateX(0px)`;
+      return;
+    }
+  
+    let images = document.getElementById("ul1");
+    images.style.transform = `translateX(-${currentSlide * 450}px)`;
+  
+    if (currentSlide === imageCount.length - 1) {
+      currentSlide = 0;
+    } else {
+      currentSlide++;
+    }
+  }
+  
+  setInterval(() => {
+    startSlider();
+  }, 5000);
